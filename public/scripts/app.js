cce1700d73504dbe131ebf8b5c0345ae1800da5c
Angela Larryant <angelarryant@gmail.com>
2018-10-17 22:24:39 +0000

    fixed input field not appearing after attempting to add a label to each field

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -8,12 +8,8 @@ $(() => {
     }
   });
 
-  let counter = $("label").siblings("label").data();
-
   $('#addOption').click(function(event) {
     event.preventDefault();
-    counter++;
-    $(<'label'>Option <br>);
     $('<input><br>').attr("type", "text").attr("name", "options").appendTo($(".test"));
   });
 });
