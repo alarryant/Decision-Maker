731e76d6de870b5ee87ad9e12ac59bee7a3374bf
Haggis Breakfast <lj.stewart00@gmail.com>
2018-10-17 14:18:43 -0700

    added addOption function into apps.js

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,10 +1,18 @@
-$(() => {
-  $.ajax({
-    method: "GET",
-    url: "/api/users"
-  }).done((users) => {
-    for(user of users) {
-      $("<div>").text(user.name).appendTo($("body"));
-    }
-  });;
+
+$(function () {
+  $(() => {
+    $.ajax({
+      method: "GET",
+      url: "/api/users"
+    }).done((users) => {
+      for (user of users) {
+        $("<div>").text(user.name).appendTo($("body"));
+      }
+    });;
+  });
+
+  $('#addOption').click(function () {
+    let input = `<input type="text" name="options"><br>`;
+    $(input).prependTo($('#addOption'));
+  });
 });
