b5ff18f2181a4be8bffd15f1b462bbfd189bb2f3
Angela Larryant <angelarryant@gmail.com>
2018-10-17 21:51:06 +0000

    attempted to fix additional input fields

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -6,5 +6,9 @@ $(() => {
     for(user of users) {
       $("<div>").text(user.name).appendTo($("body"));
     }
-  });;
+  });
+  $('#addOption').click(function(event) {
+    event.preventDefault();
+    $('Option: <input><br>').attr("type", "text").attr("name", "options").appendTo($(".test"));
+  });
 });
