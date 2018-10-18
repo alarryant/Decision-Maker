2573e5eeade8fcedd05790767814fc7dce4130ed
danesco <danesco30@gmail.com>
2018-10-18 21:56:32 +0000

    refactored some code on server

--- server.js
+++ server.js
@@ -54,20 +54,15 @@ app.post("/create", (req, res) => {
 
   //inserting poll table into database
   knex('poll').returning('*').insert({name: req.body.title, description: req.body.description, email: req.body.email, url: randomURL}).asCallback((err, rows)=> {
-    if (err) console.log(err);
+    if (err) throw err;
 
     //loop through options and add to database
     for(let i = 0; i < req.body.options.length; i ++){
       knex('option').returning('*').insert({text: req.body.options[i], votes: 0, poll_id: rows[0].id}).asCallback((err) => {
-        if (err){
-          console.log(err);
-        }
+        if (err) throw err;
       })
     }
   });
-
-
-
 
   res.redirect(`/${randomURL}/admin`);
 });
