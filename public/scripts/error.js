4b57650d668fc0ff9fabf37a2261ddeb6525f6bb
Haggis Breakfast <lj.stewart00@gmail.com>
2018-10-19 16:57:21 -0700

    error.js

--- public/scripts/error.js
+++ public/scripts/error.js
@@ -1,15 +1,14 @@
-$('#create').click(function(event) {
-  event.preventDefault();
-  // prevent submitting and reloading page
-  let inputLength = $('input').val().length;
-  // default hide error message
-  $('.error-message').slideUp();
-  // if invalid tweet upon submit show errors
-  if (tweetLength <= 0) {
-    $('.error-message')
-      .text('you gotta write something first!')
-      .slideDown();
-  } else if (tweetLength > 140) {
-    $('.error-message')
-      .text('still too long...')
-      .slideDown();
+$(() => {
+  $('#create').click(function(event) {
+    event.preventDefault();
+    let inputLength = $('input').val().length;
+    // default hide error message
+    $('.error-message').slideUp();
+    // if invalid tweet upon submit show errors
+    if (inputLength <= 0) {
+      $('.error-message')
+        .text('fill in all forms u idiot')
+        .slideDown();
+    }
+  });
+});
