4a667d613c64f5fcba83f9340810bb6cba8dc208
danesco <danesco30@gmail.com>
2018-10-19 05:21:14 +0000

    clean up server file

--- server.js
+++ server.js
@@ -115,11 +115,3 @@ app.post('/vote', (req, res) => {
 // POST /:id/vote
 // Redirect /thanks
 
-  // function bordaCount(optionArray) {
-  //   for (i = 0; i < optionArray.length; i++) {
-  //     let numberItems = optionArray.length;
-  //     let bordaScore = numberItems - i + 1;
-  //     let option = optionArray[i];
-  //     knex('option').where('text', '=', option).increment('votes', bordaScore);
-  //   }
-  // };
