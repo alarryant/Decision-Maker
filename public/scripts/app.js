6ab7759c1a3b76520aa71d859923252612b16372
danesco <danesco30@gmail.com>
2018-10-19 19:05:30 +0000

    recover app.js and polls.ejs

---
+++ public/scripts/app.js
@@ -0,0 +1,75 @@
+$(() => {
+  $.ajax({
+    method: 'GET',
+    url: '/api/users'
+  }).done(users => {
+    for (user of users) {
+      $('<div>')
+        .text(user.name)
+        .appendTo($('body'));
+    }
+  });
+
+  function resetCounter() {
+    $('label.counterText').each(function(indivCounterText, val) {
+      let increment = indivCounterText;
+      increment++;
+      $(val).text(`Option ${increment}: `);
+    });
+  }
+
+  var counter = 2;
+
+  $('#addOption').click(function(event) {
+    event.preventDefault();
+    if (counter <= 4) {
+      counter++;
+      $('<label>')
+        .attr('for', 'options')
+        .attr('class', 'counterText')
+        .text(`Option ${counter}: `)
+        .appendTo($('.test'));
+      $('<input>')
+        .attr('type', 'text')
+        .attr('name', 'options')
+        .appendTo($('.test'));
+      $('<button>Delete</button>')
+        .attr('class', 'delete')
+        .appendTo($('.test'));
+      $('<br>').appendTo($('.test'));
+    } else {
+      // implement later with css
+      $('<br><span>')
+        .attr('class', 'removeError')
+        .text(`Let's be real you don't have that many things to do. Let's limit it to 5.`)
+        .appendTo($('form'));
+    }
+  });
+
+  $('form').on('click', '.delete', function(event) {
+    event.preventDefault();
+    const $clickTarget = $(event.target);
+    $clickTarget.next().remove();
+    $clickTarget.prev().remove();
+    $clickTarget.prev().remove();
+    $clickTarget.remove();
+    $('.removeError').remove();
+    resetCounter();
+    counter--;
+  });
+
+  $(function() {
+    $('#sortable-1').sortable();
+  });
+
+   $(".vote").on('click', function(event) {
+    event.preventDefault();
+    let $headerString = $(event.target).siblings('.pollid');
+    let randomURL = $headerString.text().substring(25, 32);
+    let optionArray = [];
+   $('li').each(function(idx, li) {
+     optionArray.push($(li).context.innerText);
+    });
+    $.post("/vote", {option: optionArray, randomURL: randomURL});
+  });
+});
