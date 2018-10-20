c6f32add52457dd227245dad3260457599880b2b
Haggis Breakfast <lj.stewart00@gmail.com>
2018-10-19 17:02:27 -0700

    error message

--- public/scripts/error.js
+++ public/scripts/error.js
@@ -2,9 +2,7 @@ $(() => {
   $('#create').click(function(event) {
     event.preventDefault();
     let inputLength = $('input').val().length;
-    // default hide error message
     $('.error-message').slideUp();
-    // if invalid tweet upon submit show errors
     if (inputLength <= 0) {
       $('.error-message')
         .text('fill in all forms u idiot')
