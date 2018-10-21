20fec666ea53f0f2f06d6993184f77929e5e7097
Haggis Breakfast <lj.stewart00@gmail.com>
2018-10-20 18:18:50 -0700

    got slide function working

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,24 +1,31 @@
 (function(d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id))
-        return;
-    js = d.createElement(s);
-    js.id = id;
-    js.src = "//connect.facebook.net/en_US/all.js";
-    fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
+  var js,
+    fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s);
+  js.id = id;
+  js.src = '//connect.facebook.net/en_US/all.js';
+  fjs.parentNode.insertBefore(js, fjs);
+})(document, 'script', 'facebook-jssdk');
 
 window.fbAsyncInit = function() {
-    FB.init({
-        appId: '274590506518760',
-        status: true,
-        xfbml: true,
-        cookie: true
-    });
+  FB.init({
+    appId: '274590506518760',
+    status: true,
+    xfbml: true,
+    cookie: true
+  });
 };
 
-
 $(() => {
+  // navbar slide function
+
+  $(function() {
+    $('.header').hover(function() {
+      $(this).toggleClass('active');
+      $('#navbar').toggle('slide', { direction: 'right' }, 2000);
+    });
+  });
 
   function resetCounter() {
     $('label.counterText').each(function(indivCounterText, val) {
@@ -47,11 +54,17 @@ $(() => {
         .appendTo($('.test'));
       $('<br>').appendTo($('.test'));
       if (counter === 3) {
-        $('.test').addClass('option3').addClass('options');
+        $('.test')
+          .addClass('option3')
+          .addClass('options');
       } else if (counter === 4) {
-        $('.test').removeClass('option3').addClass('option4');
+        $('.test')
+          .removeClass('option3')
+          .addClass('option4');
       } else if (counter === 5) {
-        $('.test').removeClass('option4').addClass('option5');
+        $('.test')
+          .removeClass('option4')
+          .addClass('option5');
       }
     } else {
       // implement later with css
@@ -62,7 +75,7 @@ $(() => {
     }
   });
 
-  $("form").on('click', '.delete', function(event) {
+  $('form').on('click', '.delete', function(event) {
     event.preventDefault();
     const $clickTarget = $(event.target).parent();
     $clickTarget.next().remove();
@@ -71,9 +84,13 @@ $(() => {
     $clickTarget.remove();
     $('.removeError').remove();
     if ($('.test').hasClass('option5')) {
-      $('.test').removeClass('option5').addClass('option4');
+      $('.test')
+        .removeClass('option5')
+        .addClass('option4');
     } else if ($('.test').hasClass('option4')) {
-      $('.test').removeClass('option4').addClass('option3');
+      $('.test')
+        .removeClass('option4')
+        .addClass('option3');
     } else if ($('.test').hasClass('option3')) {
       $('.test').removeClass('option3');
     }
@@ -85,40 +102,38 @@ $(() => {
     $('#sortable-1').sortable();
   });
 
-   $(".vote").on('click', function(event) {
+  $('.vote').on('click', function(event) {
     event.preventDefault();
     let $headerString = $(event.target).siblings('#pollid');
     let randomURL = $headerString.text();
     let optionArray = [];
-   $('li').each(function(idx, li) {
-     optionArray.push($(li).context.innerText);
+    $('li').each(function(idx, li) {
+      optionArray.push($(li).context.innerText);
     });
     // $.post("/vote", {option: optionArray, randomURL: randomURL});
     $.ajax({
       url: '/api/users/vote',
       type: 'POST',
-      data: {option: optionArray, randomURL: randomURL},
-      success:function(result){
-        console.log("we are good in the succcess function",result);
-        window.location = "/api/users/thanks";
+      data: { option: optionArray, randomURL: randomURL },
+      success: function(result) {
+        console.log('we are good in the succcess function', result);
+        window.location = '/api/users/thanks';
       },
-      error: function(error){
-        console.log("we are in the error code");
+      error: function(error) {
+        console.log('we are in the error code');
       }
     }); //AJAX Call ends here.
   });
 
-
-    $('.fb-share').click(function() {
-        FB.ui({
-            method: 'feed',
-            name: '',
-            link: 'http://reddit.com',
-            picture: '',
-            description: ''
-        });
+  $('.fb-share').click(function() {
+    FB.ui({
+      method: 'feed',
+      name: '',
+      link: 'http://reddit.com',
+      picture: '',
+      description: ''
     });
-
+  });
 
   // $("form").on('click', '#create', (function(event) {
   //   event.preventDefault();
@@ -132,5 +147,5 @@ $(() => {
   //     }
   //   });
   // });
-master
+  master;
 });
