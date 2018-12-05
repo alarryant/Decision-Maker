379f1ab21d272ab74cc3b63f7cfc8bb6d0be6104
Angela Larryant <angelarryant@gmail.com>
2018-12-05 00:46:57 +0000

    fixed knexfile

--- knexfile.js
+++ knexfile.js
@@ -25,6 +25,7 @@ module.exports = {
     client: 'postgresql',
     connection: process.env.DATABASE_URL,
     migrations: {
+      directory: './db/migrations',
       tableName: 'migrations'
     },
     ssl: true
