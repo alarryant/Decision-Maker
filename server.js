d68a225f66fb0000fb6140e8ae200de2972306ad
Angela Larryant <angelarryant@gmail.com>
2018-12-05 02:23:17 +0000

    trying new routes

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ app.use(
     outputStyle: 'expanded'
   })
 );
-app.use(express.static('public'));
+app.use(express.static(__dirname + '/public'));
 
 // Mount all resource routes
 app.use('/api/users', usersRoutes(knex));
